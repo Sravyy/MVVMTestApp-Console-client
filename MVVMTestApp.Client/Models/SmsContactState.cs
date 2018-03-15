f74dbf6c12d23f847ef5685153967a6c76ad0fda
sravanthi velaga <svelaga0412@gmail.com>
2018-03-15 01:20:30 -0700

    initial commit

---
+++ MVVMTestApp.Client/Models/SmsContactState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTestApp.Client.Models
+{
+    class SmsContactState
+    {
+        public string ContactName { get; set; }
+        public int ContactMobileNumber { get; set; }
+        public string LastMessageOn { get; set; }
+        //public virtual ICollection<SmsMessage> Messages { get; set; }
+    }
+}
---
+++ MVVMTestApp.Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using MVVMTestApp.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTestApp.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new HubConnection("http://localhost:52595/");
+            var myHub = connection.CreateHubProxy("MyHub");
+
+            connection.Start().ContinueWith(task => {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("There was an error opening the connection:{0}",
+                                      task.Exception.GetBaseException());
+                }
+                else
+                {
+                    Console.WriteLine("Connected");
+                }
+
+            }).Wait();
+
+            myHub.On<List<SmsContactState>>("PassContactsToClients", contacts =>
+            {
+                Console.WriteLine(contacts);
+            });
+             myHub.Invoke<string>("GetContacts", "doesnt matter");
+
+            Console.Read();
+            connection.Stop();
+        }
+    }
+}
